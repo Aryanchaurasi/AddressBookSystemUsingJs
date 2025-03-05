d05db1c571ea01e33e44ad83894aadbbd2ac2b1e
Aryan chaurasia <aryachaurasi20@gmail.com>
2025-03-05 11:31:59 +0530

    Aryan chaurasia :REefactor - Changed inregex to validate data properly

--- src/index.js
+++ src/index.js
@@ -5,10 +5,20 @@ const Contact = require("./model/Contact.js");
 const myAddressBook = new AddressBook();
 
 try {
-    const contact1 = new Contact("Aryan", "Chaurasia", "Ballia", "Uttar Pradesh", 221715, 7081275312, "aryan@xyz.com");
+    const contact1 = new Contact(
+        "Aryan", 
+        "Chaurasia", 
+        "Ballia City", 
+        "Uttar Pradesh", 
+        "221715",  
+        "78281597081275312961", 
+        "aryan@xyz.com"
+    );
+
+    
     myAddressBook.addContact(contact1);
     console.log(myAddressBook.getContact);
 } catch (error) {
     console.log(error);
-    
+
 }
